cb31a487b4e66d9bdb92ce9041e4a77049129b80
Sumit <sumitsharmakld@gmail.com>
2023-03-18 15:58:29 +0530

    3rd

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -48,7 +48,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
         mode,
       }}
     >
-      <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
+      <ThemeProvider theme={mode === "light" ? LightTheme : LightTheme}>
         {children}
       </ThemeProvider>
     </ColorModeContext.Provider>
