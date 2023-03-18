f9b414dea2ce92b6c4ecc4edf9c237bba3c95553
Sumit <sumitsharmakld@gmail.com>
2023-03-18 15:57:10 +0530

    2nd commit

--- client/src/contexts/index.tsx
+++ client/src/contexts/index.tsx
@@ -35,7 +35,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
 
   const setColorMode = () => {
     if (mode === "light") {
-      setMode("dark");
+      setMode("light");
     } else {
       setMode("light");
     }
@@ -48,7 +48,7 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
         mode,
       }}
     >
-      <ThemeProvider theme={ LightTheme }>
+      <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
         {children}
       </ThemeProvider>
     </ColorModeContext.Provider>
